46f8f752f72da2a14c356230ab1be4e538b61b53
수민 <squirmm.be@gmail.com>
2023-05-25 20:07:09 +0900

    :rocket: feat(main) 메인 화면 구성 완료!

--- src/components/main/MainDeveloper.vue
+++ src/components/main/MainDeveloper.vue
@@ -1,39 +1,59 @@
 <template>
   <div>
-    <b-card-group style="justify-content: center; margin-top: 200px">
+    <b-card-group id="group">
       <b-card
+        id="card"
         title="수민"
         img-src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Chipmunk.png"
-        alt="Chipmunk"
-        width="25"
-        height="25"
-        tag="article"
-        style="max-width: 20rem; border-style: dashed"
-        class="mb-2"
       >
-        <b-card-text> 나는 수민 </b-card-text>
+        <b-card-text> 나는 수민! </b-card-text>
 
-        <b-button href="#" variant="primary">깃허브</b-button>
+        <b-button href="https://github.com/squirMM" variant="primary">깃허브</b-button>
       </b-card>
-
       <b-card
+        id="card"
+        title="캠퍼"
+        img-src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Camping.png"
+      >
+        <b-card-text> ^^캠퍼 오거나이제이션^^ </b-card-text>
+
+        <b-button href="https://github.com/camping-us" variant="primary">깃허브</b-button>
+      </b-card>
+      <b-card
+        id="card"
         title="상우"
         img-src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Hamster.png"
-        alt="Hamster"
-        width="25"
-        height="25"
-        tag="article"
-        style="max-width: 20rem"
-        class="mb-2"
       >
-        <b-card-text> 나는 상우 </b-card-text>
+        <b-card-text> 나는 상우! </b-card-text>
 
-        <b-button href="#" variant="primary">깃허브</b-button>
+        <b-button href="https://github.com/sktkddn777" variant="primary">깃허브</b-button>
       </b-card>
     </b-card-group>
-    세상에서 제일 멋진 사이트입니다. <br />
-    (주)우리회사이름<br />
-    @Copyright 080-2023-0525<br />
-    저작권은 없습니다.
+
+    <div display="margin-top:100px">
+      <br /><br />
+      세상에서 제일 멋진 사이트입니다. <br />
+      (주)우리회사이름<br />
+      @Copyright 080-2023-0525<br />
+      저작권은 없습니다.
+    </div>
   </div>
 </template>
+
+<style scoped>
+#group {
+  justify-content: space-evenly;
+
+  margin-top: 200px;
+}
+#card {
+  margin: auto;
+  display: inline-block;
+  max-width: 20rem;
+  border-style: dashed;
+  border-radius: 10px;
+  box-shadow: 10px 10px 5px gray;
+  background-color: #ffffbb;
+  padding: 15px;
+}
+</style>
