7f0f5e7c59dd37cba4e18229096edc75b00e6070
한상우 <sktkddn777@naver.com>
2023-05-25 19:03:26 +0900

    :rocket: feat(main): 메인화면 생성 중

---
+++ src/components/main/MainCampper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img src="@/assets/campper.png" width="100%" />
+  </div>
+</template>
+
+<style scoped>
+#logo {
+  /* line-height: 100px; */
+  display: inline-block;
+  font-family: "bitbit";
+  font-size: 300px;
+  color: #623b2a;
+}
+
+#navbar {
+  padding: 0px;
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/components/main/MainDeveloper.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-card-group style="justify-content: center; margin-top: 200px">
+      <b-card
+        title="수민"
+        img-src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Chipmunk.png"
+        alt="Chipmunk"
+        width="25"
+        height="25"
+        tag="article"
+        style="max-width: 20rem; border-style: dashed"
+        class="mb-2"
+      >
+        <b-card-text> 나는 수민 </b-card-text>
+
+        <b-button href="#" variant="primary">깃허브</b-button>
+      </b-card>
+
+      <b-card
+        title="상우"
+        img-src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Hamster.png"
+        alt="Hamster"
+        width="25"
+        height="25"
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2"
+      >
+        <b-card-text> 나는 상우 </b-card-text>
+
+        <b-button href="#" variant="primary">깃허브</b-button>
+      </b-card>
+    </b-card-group>
+    세상에서 제일 멋진 사이트입니다. <br />
+    (주)우리회사이름<br />
+    @Copyright 080-2023-0525<br />
+    저작권은 없습니다.
+  </div>
+</template>
