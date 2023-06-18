751fce070d7daaed862217e67725d6f151219136
squirMM <jsm6616@naver.com>
2023-06-19 03:53:23 +0900

    :rocket: 강제주입 해제

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 function apiInstance() {
   const instance = axios.create({
-    // baseURL: process.env.VUE_APP_API_BASE_URL,
-    baseURL: "http://campper.site/api",
+    baseURL: process.env.VUE_APP_API_BASE_URL,
+    // baseURL: "http://campper.site/api",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
