0464c5341f8d402d28c90e0ecb56f63f316a6061
squirMM <jsm6616@naver.com>
2023-06-19 23:29:18 +0900

    :hammer: refac(campInfo): 상세 투표  컴포넌트 생성 #31

---
+++ src/components/camp/item/DetailVoteInfo.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="vote-box">
+    <li>전체평점: {{ this.calculate(vote.total / vote.voteCnt) }}</li>
+    <li>
+      위치_적합도:
+      {{ this.calculate(vote.location / vote.voteCnt) }}
+    </li>
+    <li>청결도: {{ this.calculate(vote.cleanliness / vote.voteCnt) }}</li>
+    <li>친절도: {{ this.calculate(vote.kindness / vote.voteCnt) }}</li>
+    <li>가격_적합도: {{ this.calculate(vote.price / vote.voteCnt) }}</li>
+    <li>
+      부대시설_총합:
+      {{ this.calculate(vote.facilities / vote.voteCnt) }}
+    </li>
+    <button id="voteBtn" @click="openVote">
+      나도_투표하기:
+      <img
+        src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand%20gestures/Backhand%20Index%20Pointing%20Right%20Light%20Skin%20Tone.png"
+        width="25"
+        height="25"
+      />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailVoteInfo",
+  components: {},
+  props: {
+    vote: Object,
+  },
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    calculate(num) {
+      return isNaN(num) ? "0.0" : num.toFixed(1);
+    },
+    openVote() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+  watch: {
+    isOpen() {
+      this.$emit("open", this.isOpen);
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin-top: 10px;
+}
+#vote-box {
+  border-radius: 10px;
+  border: dashed;
+  box-shadow: 10px 10px 5px gray;
+  width: 80%;
+  height: 100%;
+  margin: auto;
+  padding: 15px;
+  background-color: #ffffbb;
+}
+#voteBtn {
+  border-radius: 3px;
+  margin-top: 30px;
+}
+</style>
